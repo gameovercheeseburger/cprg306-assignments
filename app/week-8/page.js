6767eb141361a36b249eb909c29238ada5fbf5a9
Patrik S <patrikjurgen.santiago@edu.sait.ca>
2024-11-06 19:00:43 -0700

    Signed-off-by: Patrik S <patrikjurgen.santiago@edu.sait.ca>

---
+++ app/week-8/page.js
@@ -0,0 +1,24 @@
+'use client';
+
+import React, { useState } from 'react';
+import ItemList from './item-list';
+import NewItem from './new-item';
+import itemsData from './items.json';
+
+const Page = () => {
+  const [items, setItems] = useState(itemsData);
+
+  const handleAddItem = (newItem) => {
+    setItems((prevItems) => [...prevItems, newItem]);
+  };
+
+  return (
+    <main className="p-8 bg-black text-white w-1/4 min-h-screen">
+      <h1 className="text-3xl font-bold mb-6">Shopping List</h1>
+      <NewItem onAddItem={handleAddItem} />
+      <ItemList items={items} />
+    </main>
+  );
+};
+
+export default Page;
